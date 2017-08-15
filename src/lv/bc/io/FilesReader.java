9fd94e3fbd3a284b6e63c6a0853bab46f673edb1
Ketevane Dzeluri <ketedzel@gmail.com>
2017-08-15 17:50:18 +0300

    FileReader update

--- src/lv/bc/io/FilesReader.java
+++ src/lv/bc/io/FilesReader.java
@@ -12,27 +12,49 @@ public class FilesReader {
 	private String fileName;
 	//TODO: save list of 4 elements members
 	private List<Topic> topicList;
+	//TODO: save in parameter the Topic for translation
+	private Topic currWord;
 	
-	private int pointer;
+	private int pointer = 0;
     private int totalGroupAnswers = 0;
 	public FilesReader(String fileName) {
-		super();
-		this.fileName = fileName;
-		this.pointer = 0;
+		//TODO: delete this hardcode later later
+		fileName  = "Animal.txt";
+		setTopicList();
+		setCurrWord();
+		//TODO: this.fileName = fileName and has to be only login inside initializeTopic
+		//	initializeTopic();
+	}
+	public void setTopicList() {
 		topicList = new ArrayList<Topic>();
-		try {
-			initializeTopic();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		topicList.add(new Topic("suns", "dog", 0));
+		topicList.add(new Topic("lauva", "lion", 0));
+		topicList.add(new Topic("putns", "bird", 0));
+		topicList.add(new Topic("kengurs", "kangaroo", 0));
+	}
+	
+	public List<Topic> getTopicList() {
+		return topicList;
 	}
 
-	// read from necessary file and row the word
-	// and fill in textFrom and textTo
+	public String getCurrWord() {
+		return currWord.getFromText();
+	}
+
+	public void setCurrWord() {
+		currWord = new Topic("suns", "dog", 0);
+	}
+	
+	public boolean checkAnswer(Topic answerTopic){
+		return true;
+	}
+	
+
 	//1:TODO: change initialize so that it will read into List any read 4 new words from file 
+	// read from necessary file next 4 words and place it into topicList
+	// save currWord object
 	public void initializeTopic() throws IOException{
-		Path path = Paths.get(this.fileName);
+        Path path = Paths.get(this.fileName);
     	if (Files.notExists(path)) {
     		System.exit(1);
     	}
@@ -49,7 +71,7 @@ public class FilesReader {
 	    	int txtCnt = 0;
 	        while((line = reader.readLine()) != null && i < 12) {
             	if(l == 3){
-            		//1:Save previous Topic to Array
+            		//1: TODO: then you will the file will work Save previous Topic to Array
             		topicList.add(new Topic(txtFrom, txtTo, txtCnt));
             		l = 0;
             	}	
@@ -62,7 +84,9 @@ public class FilesReader {
             	l++;
             	i++;
             }
-	    	    this.pointer = i;// hast to be 12 for first time or less if in file is less then 4 words
+	        //TODO: chage this hard for first init save word from 1st Array position
+	        currWord = new Topic("suns", "dog", 0);
+	        this.pointer = i;// hast to be 12 for first time olr less if in file is less then 4 words
 	    }
 	    finally {
 	           reader.close();
@@ -71,12 +95,13 @@ public class FilesReader {
 	}
 	    
     	
-    	//2:make random method to mix 2nd, 3rd, 4th Topic in List
-	
-	// make logic if answer success then 
-	// regenrate random and save count +1 to necessary array object,
-	// if not, then just random regeneration 
-	// if all words in list was passed, then
+    //2: TODO: mixList mix the Topic object int the topicList
+	// 2.1: check was answer correct
+	// 2.2: check that 12 times correwct answer was reached
+	// 2.3: if answer was correct, then currWord has to be changed on Word who is in topicList and have count < 3
+	// 2.4: if answer was incorrect , then just rebuild order of objects in topicList
+	// 2.5: if successed 12 correct answers, then call initializeTopics()
+
 	public void mixList(String answer){
 		if(topicList.get(0).equals(answer)){
 		   
