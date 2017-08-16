96d946beb5f5d1a3bc1ef53241b12a9cc0c905a5
Igors Kreknins <ikreknin@gmail.com>
2017-08-16 05:48:49 -0400

    correction in main

--- src/lv/bc/Main.java
+++ src/lv/bc/Main.java
@@ -12,7 +12,7 @@ public class Main {
 			@Override
 			public void run() {
 				Model model = new Model(0);
-				View view = new View("Label");
+				View view = new View("");
 				Controller controller = new Controller(model, view);
 				controller.contol();
 			}
