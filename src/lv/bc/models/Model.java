bf005b29029168a0deea65e9aba8f2c4ff4bc943
Shokhruz Sattarov <shokhruz.sattarov@gmail.com>
2017-08-16 10:37:56 +0300

    Methods to model for File| Mode| Options| Help items added

--- src/lv/bc/models/Model.java
+++ src/lv/bc/models/Model.java
@@ -19,4 +19,81 @@ public class Model {
 	public int getX() {
 		return x;
 	}
+
+	// Buttons' methods ------------------------------------------------------------------------------------------------------
+	public void doQuestion() {
+
+	}
+
+	public void doQuestion1() {
+
+	}
+
+	public void doQuestion2() {
+
+	}
+
+	public void doQuestion3() {
+
+	}
+
+	public void doQuestion4() {
+
+	}
+
+	// Main "File" Menu Items ------------------------------------------------------------------------------------------------------
+
+	// Sub-Menu "Open" method
+				public void doOpen() {
+
+	}
+
+	// Sub-Menu "Save" method
+				public void doSave() {
+
+	}
+
+	// Sub-Menu "Reset" method
+				public void doReset() {
+
+	}
+
+	// Main Menu "Mode" Items------------------------------------------------------------------------------------------------------
+
+	// Sub-menu "Silent" method
+				public void doSilent() {
+
+	}
+	
+	// Sub-menu "Audio" method
+				public void doAudio() {
+
+		}
+	
+		// Sub-menu "Text" method
+				public void doText() {
+
+						}
+		
+		// Sub-menu "Foreign-to-Native" method
+				public void doFN() {
+
+								}	
+		// Sub-menu "Native-to-Foreign" method
+				public void NF() {
+
+										}	
+				
+		// Main Menu "Options" Items------------------------------------------------------------------------------------------------------	
+				// Sub-menu "Language" method
+				
+				public void doLanguage() {
+
+										}
+				
+		// Main Menu "Help" method------------------------------------------------------------------------------------------------------	
+		
+				public void doHelp() {
+
+									}
 }
