1035d0d60cff894340dc9019f3eadb1079fa9000
burgeriitis <s.krauja@gmail.com>
2017-08-16 23:17:36 +0300

    Settings are handled with Properties class

--- src/lv/bc/models/Settings.java
+++ src/lv/bc/models/Settings.java
@@ -1,85 +1,82 @@
 package lv.bc.models;
 
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
 
 public class Settings {
 	
-	
-	private String foreignLanguage;	
-	private int lastLine;
+	//Strings for property names
+	public static String foreignLanguage = "foreignLanguage";
+	public static String lastLine = "lastLine";
+
 	private String iniFile = "src/lv/bc/models/Settings.ini";
 	Path path = Paths.get("src/lv/bc/models/Settings.ini");
 	protected final int SETTING_COUNT = 2;
+	Properties appProperties = new Properties();
 	
-	Settings() throws IOException  {
-		String[] iniParameters = new String[SETTING_COUNT];
-		
+	Settings()  {
 		
 		if (Files.exists(path)) {
-			 try(BufferedReader in = new BufferedReader(
-					 new FileReader(iniFile))){
-				 String line = in.readLine();
-				 int i = 0;
-				 while(line != null){
-					 iniParameters[i] = line;
-					 i++;
-					 line = in.readLine();
-				 }
-			 }
-			 catch(IOException e){
-				 System.out.println(e);
-			 }
-			 
-			 this.foreignLanguage = iniParameters[0];
-			 this.lastLine = Integer.parseInt(iniParameters[1]);
+			FileInputStream in;
+			try {
+				in = new FileInputStream(iniFile);
+				appProperties.load(in);
+				in.close();
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println(appProperties.getProperty(foreignLanguage));
+			
+			//What to do, if in file is not enough properties?
+			//One thing we can do- make default property file
 		}
 		else {
-			Files.createFile(path);
-			this.foreignLanguage = "English";
-			this.lastLine = 0;
+			try {
+				Files.createFile(path);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			appProperties.setProperty(foreignLanguage, "English");
+			appProperties.setProperty(lastLine, "0");
 		}
-		
-		
+	}	
+	
+	//TODO For setting new property values you need to use setProperty(String key, String value)
+	//TODO call on EXIT controller
+	public void saveAndExit() {
+		try {
+			FileOutputStream out = new FileOutputStream(iniFile);
+			appProperties.store(out, "Looool");
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
-	public String getForeignLanguage() {
-		return foreignLanguage;
-	}
-
-	public void setForeignLanguage(String foreignLanguage) {
-		this.foreignLanguage = foreignLanguage;
-	}
-
-	public int getLastLine() {
-		return lastLine;
-	}
-
-	public void setLastLine(int lastLine) {
-		this.lastLine = lastLine;
-	}
-
-	public String getIniFile() {
-		return iniFile;
-	}
-
-	public void setIniFile(String iniFile) {
-		this.iniFile = iniFile;
-	}
-
-	
-	public static void main(String[] args) throws IOException {
+	public static void main (String[] args) {
 		Settings set = new Settings();
+		set.appProperties.setProperty(foreignLanguage, "Russian");
+		System.out.println(set.appProperties.getProperty(foreignLanguage));
+		set.saveAndExit();
+		
 	}
-	
-	
 	
 }
+
+
+
+
+
+
+
